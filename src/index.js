dd7e3e129ca2ab76aea82a8199bdae90859203e3
Jigar Gosar <jigar.gosar@gmail.com>
2019-04-23 13:49:10 +0530

    init canvas

--- src/index.js
+++ src/index.js
@@ -31,3 +31,9 @@ function setCanvasSize(w, h, canvas) {
   canvas.width = w
   canvas.height = h
 }
+
+const Rect = {
+  create: (x, y, w, h) => ({ x, y, w, h }),
+  getWidth: ({ w }) => w,
+  getHeight: ({ h }) => h,
+}
