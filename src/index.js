1846421d773308355256f1732c1f37e38f00a9d0
Jigar Gosar <jigar.gosar@gmail.com>
2019-04-23 13:18:43 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import { observer } from "mobx-react";
-import ReactDOM from "react-dom";
-import "tachyons";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'tachyons'
+import './index.css'
 
-const App = observer(function App() {
+const App = function App() {
   return (
     <div className="w-80 center sans-serif">
       <div className="mt3 f4 ttu tracked">HEADER</div>
     </div>
-  );
-});
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
