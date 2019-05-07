6c6979be8dfc6b96a63532611b13a2cb339a1473
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-07 16:00:52 +0530

    ts

--- src/v5/Point.ts
+++ src/v5/Point.ts
@@ -1,8 +1,25 @@
+import * as R from 'ramda'
+
 type Point = {
   x: number
   y: number
 }
 
-function len(p1: Point, p2: Point) {
-  return
+type ReducerNum2 = (a: number, b: number) => number
+
+function map2Both(fn: ReducerNum2, a: Point, b: Point): Point {
+  return { x: fn(a.x, b.x), y: fn(a.y, b.y) }
+}
+
+function subtract(a: Point, b: Point): Point {
+  return map2Both(R.subtract, a, b)
+}
+
+function square(a: number): number {
+  return a * a
+}
+
+function len(a: Point, b: Point) {
+  const c = subtract(a, b)
+  return Math.sqrt(square(c.x) + square(c.y))
 }
