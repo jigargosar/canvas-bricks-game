67e0babaac82429b01e07510e2db98fbdad473d3
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-17 15:44:46 +0530

    v7-kata-1

--- src/v7-kata-1.js
+++ src/v7-kata-1.js
@@ -32,17 +32,32 @@ function initCanvas(width, height) {
   return ctx
 }
 
+const initKeyboard = () => {
+  const keyDownDict = {}
+  window.addEventListener('keydown', e => (keyDownDict[e.key] = true))
+  window.addEventListener('keyup', e => (keyDownDict[e.key] = false))
+  return {
+    get left() {
+      return keyDownDict['ArrowLeft']
+    },
+    get right() {
+      return keyDownDict['ArrowRight']
+    },
+  }
+}
+
 const run = initFn => viewFn => updateFn => {
   const vp = { x: 0, y: 0, w: 400, h: 400 }
 
   const ctx = initCanvas(vp.w, vp.h)
+  const key = initKeyboard()
 
   const initialState = initFn(vp)
 
   const onFrame = state => () => {
     const viewCmds = viewFn(state)
     Canvas2D.run({ ctx, vp })(viewCmds)
-    const nextState = updateFn(vp)(state)
+    const nextState = updateFn({ vp, key })(state)
     requestAnimationFrame(onFrame(nextState))
   }
 
@@ -57,7 +72,7 @@ const initPad = vp => {
   const w = 100
   const h = 15
 
-  return { x: (vp.w - w) / 2, y: vp.h - h * 2, w, h }
+  return { x: (vp.w - w) / 2, y: vp.h - h * 2, w, h, dx: 0, dy: 0 }
 }
 
 const view = state => [
@@ -71,8 +86,13 @@ const viewPad = pad => {
   return Canvas2D.fillRect({ x, y, w, h, style: 'orange' })
 }
 
-const update = vp => state => {
-  return state
+const update = ({ vp, key }) => state => {
+  return R.compose(updatePadVel(key))(state)
 }
+
+const arrowKeyToUnitDx = key => (key.left ? -1 : key.right ? 1 : 0)
+
+const updatePadVel = key => state =>
+  R.assocPath(['pad', 'dx'], arrowKeyToUnitDx(key) * 10)(state)
 
 run(init)(view)(update)
