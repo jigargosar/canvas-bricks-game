c2f00856fa4703f6f7c0182fc7ead4a47a87f372
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-17 10:24:21 +0530

    v7-kata-1

--- src/v7-kata-1.js
+++ src/v7-kata-1.js
@@ -11,6 +11,20 @@ const run = () => {
   canvas.width = 400
   canvas.height = 400
   canvas.className = 'db center ba bw1 b--green'
+
+  const ctx = canvas.getContext('2d')
+
+  const state = {
+    pad: { x: 0, y: 0, w: 100, h: 15 },
+  }
+
+  render(ctx)(state)
+}
+
+const render = ctx => state => {
+  const { x, y, w, h } = state.pad
+  ctx.fillStyle = 'orange'
+  ctx.fillRect(x, y, w, h)
 }
 
 run()
